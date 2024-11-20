007f846cd2227a3bd612eab671be1ade7c5a408b
theMANGOlorian <thechild.and.themango@gmail.com>
2024-11-20 11:58:27 +0100

    changed Go PATH from: fantastic-coffee to: theMANGOlorian/WASAtext

--- service/api/api.go
+++ service/api/api.go
@@ -38,7 +38,7 @@ package api
 
 import (
 	"errors"
-	"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/database"
+	"github.com/theMANGOlorian/WASAtext/service/database"
 	"github.com/julienschmidt/httprouter"
 	"github.com/sirupsen/logrus"
 	"net/http"
